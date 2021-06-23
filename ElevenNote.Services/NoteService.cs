f1934404a5c75d9c430265dbf31ff548d7830f4a
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-23 18:45:47 -0500

    Add NoteService with Create and Get methods

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,46 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        private readonly Guid _userId;
+        private ApplicationDbContext _ctx = new ApplicationDbContext();
+
+        public NoteService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateNote(NoteCreate model)
+        {
+            var entity = new Note()
+            {
+                OwnerId = _userId,
+                Title = model.Title,
+                Content = model.Content,
+                CreatedUtc = DateTimeOffset.Now
+            };
+            _ctx.Notes.Add(entity);
+            return _ctx.SaveChanges() == 1;
+        }
+
+        public IEnumerable<NoteListItem> GetNotes()
+        {
+            var query = _ctx.Notes.Where(e => e.OwnerId == _userId)
+                                  .Select(e => new NoteListItem
+                                  {
+                                      NoteId = e.NoteId,
+                                      Title = e.Title,
+                                      CreatedUtc = e.CreatedUtc
+                                  });
+            return query.ToArray();
+        }
+    }
+}
