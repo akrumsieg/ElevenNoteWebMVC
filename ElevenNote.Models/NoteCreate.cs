04c5bb5f98ff295e6e15baf9274f350d5686fc9f
Aaron Krumsieg <a.krumsieg@gmail.com>
2021-06-23 18:10:03 -0500

    Add NoteCreate model -- Add Get Create() method

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        [MinLength(2, ErrorMessage = "Titles must have 2-40 characters.")]
+        [MaxLength(40, ErrorMessage = "Titles must have 2-40 characters.")]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Notes must have 1-1000 characters.")]
+        [MaxLength(1000, ErrorMessage = "Notes must have 1-1000 characters.")]
+        public string Content { get; set; }
+    }
+}
